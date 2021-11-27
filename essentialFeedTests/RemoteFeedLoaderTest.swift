462bf8ffb856431c889030b0b04a9f1c4abb00f6
mathieu janneau <mjanneau@cityway.fr>
2021-11-27 19:06:14 +0100

    singleton implementation

--- essentialFeedTests/RemoteFeedLoaderTest.swift
+++ essentialFeedTests/RemoteFeedLoaderTest.swift
@@ -8,20 +8,31 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.sharedInstance.requestedURL = URL(string: "http://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let sharedInstance = HTTPClient()
+    private init() {}
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTest: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.sharedInstance
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
     }
 
+    func test_init_doesRequestDataFromURL() {
+        let client = HTTPClient.sharedInstance
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
+    }
 }
