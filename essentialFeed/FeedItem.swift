b471629dd1f17e3557d65a20b8c6ab0f645c5e7b
mathieu janneau <mjanneau@cityway.fr>
2021-11-27 16:56:14 +0100

    create feedItem

---
+++ essentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  essentialFeed
+//
+//  Created by Mathieu Janneau on 27/11/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL?
+}
