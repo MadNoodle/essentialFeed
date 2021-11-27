cde7ce0ac22fc761fd7227e512fd192a3f1e8776
mathieu janneau <mjanneau@cityway.fr>
2021-11-27 16:54:13 +0100

    Initial Commit

---
+++ essentialFeed/essentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  essentialFeed.h
+//  essentialFeed
+//
+//  Created by Mathieu Janneau on 27/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for essentialFeed.
+FOUNDATION_EXPORT double essentialFeedVersionNumber;
+
+//! Project version string for essentialFeed.
+FOUNDATION_EXPORT const unsigned char essentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <essentialFeed/PublicHeader.h>
+
+
