f6b175a35294a743d7a299f0f3f32e39f325bff8
mathieu janneau <mjanneau@cityway.fr>
2021-11-27 19:34:01 +0100

    move implementation in target

--- essentialFeed/FeedFeature/FeedLoader.swift
+++ essentialFeed/FeedFeature/FeedLoader.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 enum LoadFeedResult {
     case success([FeedItem])
     case error(Error)
@@ -15,4 +14,23 @@ enum LoadFeedResult {
 
 protocol FeedLoader {
     func load(completion: @escaping((LoadFeedResult) -> Void))
+}
+
+class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init (url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+// permet de faire une extension de Alamofire ou URLSession
+protocol HTTPClient {
+    func get(from url: URL)
 }
