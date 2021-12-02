5a072958cc0818cdff2f9101eb1aad7c748c7671
mathieu janneau <mjanneau@cityway.fr>
2021-12-02 13:42:22 +0100

    simplify type declarations with a helper method

--- essentialFeedTests/RemoteFeedLoaderTest.swift
+++ essentialFeedTests/RemoteFeedLoaderTest.swift
@@ -38,7 +38,7 @@ class RemoteFeedLoaderTest: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSut()
         
-        expect(sut, toCompleteResult: .failure(RemoteFeedLoader.Error.connectivity)) {
+        expect(sut, toCompleteResult: failure(.connectivity)) {
             let clientError = NSError(domain: "test", code: 0)
             client.complete(with: clientError)
         }
@@ -50,7 +50,7 @@ class RemoteFeedLoaderTest: XCTestCase {
         
         let samples = [199, 201, 300, 400, 500]
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut, toCompleteResult: failure(.invalidData)) {
                 let json = makeItemsJson([])
                 client.complete(withStatusCode: code,data: json, at: index)
             }
@@ -59,7 +59,7 @@ class RemoteFeedLoaderTest: XCTestCase {
     
     func test_load_delivers_anInvalidJSON() {
         let (sut, client) = makeSut()
-        expect(sut, toCompleteResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteResult: failure(.invalidData)) {
             let invalidJson = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJson)
         }
@@ -138,6 +138,10 @@ class RemoteFeedLoaderTest: XCTestCase {
     private func makeItemsJson(_ items: [[String: Any]]) -> Data {
         let json = ["items": items]
         return try! JSONSerialization.data(withJSONObject: json)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func expect(_ sut: RemoteFeedLoader, toCompleteResult expectedResult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
